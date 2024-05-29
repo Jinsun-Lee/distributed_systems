12d205c43b6f666f2239303fd9e8ce3eafa5a243
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2024-05-30 01:04:03 +0900

    v.3.1.1 - "schedule() - all comments"
    - wg.Add(ntasks)랑 wg.Add(1)은 차이X
    - 전자는 79s, 106s
    - 후자는 81s, 88s
    - 후자가 더 안전하다고 함

--- sequential_mapreduce/src/mapreduce/schedule.go
+++ sequential_mapreduce/src/mapreduce/schedule.go
@@ -37,6 +37,15 @@ func (mr *Master) schedule(phase jobPhase) {
 
 	// Use a WaitGroup to wait for all tasks to complete
 	var wg sync.WaitGroup                 // 고루틴이 모두 종료될 때까지 기다리려고 사용 
+
+	/*
+	ntasks만큼의 고루틴을 생성하지 않고,  
+	taskChannel 채널의 버퍼 크기를 ntasks로 설정한 뒤 고루틴을 실행 = 한 번에 최대 ntasks개의 고루틴이 실행될 수 있음
+	각 고루틴이 시작되기 전에 작업 수를 추가함
+	*/
+
+	//wg.Add(ntasks)
+
 	taskChannel := make(chan int, ntasks) // ntasks 크기의 버퍼가 있는 정수 채널 taskChannel을 생성(이 채널로 작업 관리)
     for i := 0; i < ntasks; i++ {         // ntasks만큼의 숫자를 채널에 보냄 각 작업의 번호를 나타내고
         taskChannel <- i
@@ -44,12 +53,23 @@ func (mr *Master) schedule(phase jobPhase) {
     close(taskChannel)                    // 모든 작업 번호를 채널에 보낸 후 채널을 닫음
 
 	
-	for i := 0; i < ntasks; i++ {                  // ntasks만큼의 고루틴을 생성
-        wg.Add(1)                                  // 각 고루틴마다 한 번만 호출하여 WaitGroup을 올바르게 초기화, 모든 고루틴이 종료될 때까지 기다리는 작업의 수를 추가하는 것
-        go func() {
+	for i := 0; i < ntasks; i++ {         // ntasks만큼의 고루틴을 생성
+        
+		/*
+		각 고루틴마다 한 번만 호출하여 WaitGroup을 올바르게 초기화
+		모든 고루틴이 종료될 때까지 기다리는 작업의 수를 추가하는 것
+		
+		각 고루틴이 시작될 때마다 작업수를 추가
+		ntasks 만큼의 고루틴을 생성하고, 각 고루틴이 taskChannel에서 작업을 수행 
+	    = 채널 버퍼에 따른 제한 없이 ntasks 만큼의 고루틴이 동시에 실행 가능
+		*/
+		
+		wg.Add(1)                                  
+        
+		go func() {
             defer wg.Done()                        // 각 고루틴이 작업 완료 후 WaitGroup에 알림
 
-            for taskNumber := range taskChannel {  // 각 고루틴은 taskChannel에서 작업을 가져옴( 작업번호를 가져와 작업 수행)
+            for taskNumber := range taskChannel {  // 각 고루틴은 taskChannel에서 작업을 가져옴(작업번호를 가져와 작업 수행)
                 worker := <-mr.registerChannel     // 워커를 가져와 작업을 할당
 
                 args := DoTaskArgs{
