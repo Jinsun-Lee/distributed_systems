c6ff4016f7cf72476f41f01c22240f5aad6c36a3
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2024-05-29 22:51:56 +0900

    v.3.1.0 - "schedule() - 88s and comments"

--- sequential_mapreduce/src/mapreduce/schedule.go
+++ sequential_mapreduce/src/mapreduce/schedule.go
@@ -6,16 +6,19 @@ import (
 )
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
+// Master 구조체의 메서드로 정의 - jobPhase(작업의 종류 - mapPhase, reducePhase) 타입의 phase 인자를 받음
 func (mr *Master) schedule(phase jobPhase) {
-	var ntasks int
-	var nios int // number of inputs (for reduce) or outputs (for map)
+
+	var ntasks int // 실행할 작업의 수를 저장
+	var nios int   // I/O를 저장할 변수로 reduce면 입력 파일의 수, map이면 출력 파일의 수
+	
 	switch phase {
-	case mapPhase:
-		ntasks = len(mr.files)
-		nios = mr.nReduce
-	case reducePhase:
-		ntasks = mr.nReduce
-		nios = len(mr.files)
+		case mapPhase:
+			ntasks = len(mr.files) // 맵 작업의 수 = 입력 파일의 수
+			nios = mr.nReduce      // 각 맵 작업이 생성할 리듀스 작업의 수
+		case reducePhase:
+			ntasks = mr.nReduce    // 리듀스 작업의 수 
+			nios = len(mr.files)   // 리듀스 작업의 입력이 되는 중간 파일의 수
 	}
 
 	debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
@@ -33,41 +36,45 @@ func (mr *Master) schedule(phase jobPhase) {
 
 
 	// Use a WaitGroup to wait for all tasks to complete
-	var wg sync.WaitGroup
-	wg.Add(ntasks)
+	var wg sync.WaitGroup                 // 고루틴이 모두 종료될 때까지 기다리려고 사용 
+	taskChannel := make(chan int, ntasks) // ntasks 크기의 버퍼가 있는 정수 채널 taskChannel을 생성(이 채널로 작업 관리)
+    for i := 0; i < ntasks; i++ {         // ntasks만큼의 숫자를 채널에 보냄 각 작업의 번호를 나타내고
+        taskChannel <- i
+    }
+    close(taskChannel)                    // 모든 작업 번호를 채널에 보낸 후 채널을 닫음
 
-    for i := 0; i < ntasks; i++ {
-        wg.Add(1)
-        go func(taskNumber int) {
-            defer wg.Done()
+	
+	for i := 0; i < ntasks; i++ {                  // ntasks만큼의 고루틴을 생성
+        wg.Add(1)                                  // 각 고루틴마다 한 번만 호출하여 WaitGroup을 올바르게 초기화, 모든 고루틴이 종료될 때까지 기다리는 작업의 수를 추가하는 것
+        go func() {
+            defer wg.Done()                        // 각 고루틴이 작업 완료 후 WaitGroup에 알림
 
-            worker := <-mr.registerChannel
-            
-			args := DoTaskArgs{
-                JobName:       mr.jobName,
-                Phase:         phase,
-                TaskNumber:    taskNumber,
-                NumOtherPhase: nios,
-                File:          mr.files[taskNumber],
+            for taskNumber := range taskChannel {  // 각 고루틴은 taskChannel에서 작업을 가져옴( 작업번호를 가져와 작업 수행)
+                worker := <-mr.registerChannel     // 워커를 가져와 작업을 할당
+
+                args := DoTaskArgs{
+                    JobName:       mr.jobName,
+                    Phase:         phase,
+                    TaskNumber:    taskNumber,
+                    NumOtherPhase: nios,
+                    File:          mr.files[taskNumber],
+                }
+
+                ok := call(worker, "Worker.DoTask", &args, new(struct{}))
+                if !ok {
+                    fmt.Printf("Schedule: RPC %s DoTask error\n", worker)
+                    taskChannel <- taskNumber     // 작업이 실패하면 작업을 taskChannel에 넣어 재할당
+                }
+
+                go func() {
+                    mr.registerChannel <- worker // 작업이 완료되면 워커를 registerChannel로 반환
+                }()
             }
-            
-			ok := call(worker, "Worker.DoTask", &args, new(struct{}))
-            if !ok {
-                fmt.Printf("Schedule: RPC %s DoTask error\n", worker)
-            }
-            
-			// task fin > add worker to channel
-            go func() {
-                mr.registerChannel <- worker
-            }()
-
-        }(i)
+        }()
     }
 
-    // wait all task done
-    wg.Wait()
+    wg.Wait() // 모든 고루틴이 작업을 마치면 고루틴이 종료될 때까지 대기
 
-
-	// END
-	debug("Schedule: %v phase done\n", phase)
+    // END
+    debug("Schedule: %v phase done\n", phase)
 }
